817e9ed97873f01275954b3da4915dbf7dd6995d
Gagan Singh <gagansingh010221@gmail.com>
2025-07-15 23:41:09 +0530

    Fix shell command path error

--- AshBrick/src/testSupabase.js
+++
@@ -1,8 +0,0 @@
-import { supabase } from "./lib/supabase";
-
-supabase
-  .from('profiles')
-  .select('*')
-  .limit(1)
-  .then(res => console.log("Test Supabase query result (standalone):", res))
-  .catch(err => console.error("Test Supabase query error (standalone):", err));
